04fa10ea47cdd0f28cd4709e7a0bb945a856c12a
Eryk130819 <63423813+Eryk130819@users.noreply.github.com>
2024-01-14 11:19:41 +0100

    Native Login Test

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void cleanup(){
+        driver.quit();
+    }
+}
